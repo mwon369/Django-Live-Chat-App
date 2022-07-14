b8ba306624566629c049803b0bdeba6d192007f7
Mitchell <mitchellwong833@gmail.com>
2022-07-14 22:05:00 +1200

    Setup the chat room html page and URL endpoints.

--- djangochat/room/views.py
+++ djangochat/room/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Room
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# use decorator to ensure user is logged in
+@login_required
+def rooms(request):
+    # render the list of all rooms we have in the database
+    rooms = Room.objects.all()
+    return render(request, 'room/rooms.html', {'rooms': rooms})
