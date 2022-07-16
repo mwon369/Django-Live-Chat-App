4da32b81ea6da49f23cc9c2b3fb5214458c18cb3
Mitchell <mitchellwong833@gmail.com>
2022-07-16 17:59:38 +1200

    Implemented functionality to allow sent messages to be stored in the database

--- djangochat/room/consumers.py
+++ djangochat/room/consumers.py
@@ -1,6 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer # used to create consumer
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
+from .models import Message, Room
+from django.contrib.auth.models import User
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -31,6 +33,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
+        # sync function will finish saving the message to the database before sending to the chatroom
+        await self.save_message(username, room, message)
+
         # send the message to other consumers listening to the same group/channel
         # this will allow different instances of the application to talk to each other
         await self.channel_layer.group_send(
@@ -55,3 +60,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'username': username,
             'room': room,
         }))
+
+    # makes it possible to store data in the database while we await for the async functions
+    @sync_to_async
+    def save_message(self, username, room, message):
+        # get user/room data based on the username and room (slug) that is passed in
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+        # save the message to the database
+        Message.objects.create(user=user, room=room, content=message)
